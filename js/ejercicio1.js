792cd6412f5eb7557e1ccd2a02a9631f4c9d3453
jhonnmv19 <ajhonn17mv@gmail.com>
2025-12-11 09:56:07 -0400

    Actualización del index y nuevas imágenes

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,8 +1,7 @@
-// EJERCICIO 1: Temperatura → Clima
+
 function ejercicio1() {
     const valor = document.getElementById("tempInput").value;
 
-    // Validación si está vacío
     if (valor.trim() === "") {
         document.getElementById("resultado1").textContent = "⚠ Por favor ingrese una temperatura.";
         return;
@@ -10,7 +9,6 @@ function ejercicio1() {
 
     const temp = parseFloat(valor);
 
-    // Validación si no es número
     if (isNaN(temp)) {
         document.getElementById("resultado1").textContent = "⚠ Ingrese un número válido.";
         return;
